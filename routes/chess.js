6709947f08d7e736eb14c73004cbb38d2de24429
David O'Mahony <domahony@compusult.net>
2016-01-09 22:43:13 -0330

    More updates

--- routes/chess.js
+++ routes/chess.js
@@ -3,38 +3,55 @@
  * GET users listing.
  */
 
+const util = require('util');
+const EventEmitter = require('events');
+
 var fs = require('fs');
 var spawn = require('child_process').spawn;
 
-var child = spawn(
+function Game() {
 
-	'/usr/games/gnuchess',
-	['--uci'],
-	{
-    		stdio: ['pipe', 'pipe', 'pipe'],
+	EventEmitter.call(this);
+
+	this.child = spawn(
+		'/usr/games/gnuchess', ['--uci'],
+		{
+    			stdio: ['pipe', 'pipe', 'pipe'],
+		}
+	);
+
+	var gameScope = this;
+	this.child.stdout.on("data", function(data) {
+		console.log("Data1: " + data);
+		var regexp = /bestmove\s(\S+)($|\s)/;
+		var match = regexp.exec(data);
+
+		if (match != null) {
+			gameScope.emit("bestmove", match[1]);
+		}
+	});
+
+	this.write = function(cmd) {
+		console.log(cmd);
+		this.child.stdin.write(cmd, 'utf8');
 	}
-);
+}
 
+util.inherits(Game, EventEmitter);
 
-
-child.stderr.on('data', function (data) {
-	console.log('Err: ' + data);
-});
+var theGame = new Game();
 
 exports.move = function(req, res){
 
 	var cmd = "position moves " + req.params.cmd + "\n";
 	cmd += "go movetime 2000\n"; 
 
-	child.stdin.write(cmd, 'utf8', function() {
+	var cb = function(data) {
+		console.log("Data2: " + data);
+		res.send(data);
+		theGame.removeListener("bestmove", cb);
+	};
 
-		var buf = '';
-		child.stdout.on('data', function (data) {
-			buf += data;	
-			if (buf.indexOf("bestmove") != -1) {
-				res.send(buf);
-			}
-		});
-
-	});
+	theGame.on("bestmove", cb); 
+	theGame.write(cmd);
 };
